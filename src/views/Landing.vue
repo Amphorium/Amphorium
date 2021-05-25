889f503f4ed0551c95a6de2adedb3a2d84861292
Yurina22 <work.yurina@gmail.com>
2021-05-25 12:15:35 +0300

    empty project

---
+++ src/views/Landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <landing-header></landing-header>
+    <landing-main></landing-main>
+    <landing-footer></landing-footer>
+  </div>
+</template>
+
+<script>
+  import LandingFooter from "../components/landing/LandingFooter";
+  import LandingHeader from "../components/landing/LandingHeader";
+  import LandingMain from "../components/landing/LandingMain";
+  export default {
+    name: 'Landing',
+    components: {LandingMain, LandingHeader, LandingFooter}
+  }
+</script>
