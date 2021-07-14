3210a757a0ef912fc9c4e877ef082bcd19f49055
timpuyda <31927275+timpuyda@users.noreply.github.com>
2021-07-14 14:18:22 +0300

    z-index

--- src/components/preloader.vue
+++ src/components/preloader.vue
@@ -9,7 +9,7 @@
         display: flex;
         justify-content: center;
         align-items:center;
-        z-index: 1500;
+        z-index: 1500000;
         left: 0;top: 0;right: 0;bottom: 0;
         background: rgba(0,0,0,0.95);
         // background: rgba(255,255,255,0.3);
