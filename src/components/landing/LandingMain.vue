a7718c176982d8953b474ba9412ec41f66caeaa1
ArseniiOl <55720143+ArseniiOl@users.noreply.github.com>
2021-09-21 15:18:09 +0300

    add open btn to audit card

--- src/components/landing/LandingMain.vue
+++ src/components/landing/LandingMain.vue
@@ -622,8 +622,16 @@
                     </div>
                     <div class="audit-card__title"><p>As of August 1, 2021, we have passed the Ambisafe audit.</p></div>
                     <div class="audit-card__text">
-                      <p>(10mB, pdf)</p>
+                      <p>(54kB, pdf)</p>
                     </div>
+
+                    <a target="_blank" href="https://amphorium.io/docs/audit" class="audit-card__btn">
+                      Open
+                      <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8 11L13 6L8 1M1 11L6 6L1 1" stroke="#F67D75" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                      </svg>
+
+                    </a>
                   </div>
                 </div>
               </div>
